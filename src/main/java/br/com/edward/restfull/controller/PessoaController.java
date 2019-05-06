f853f62d49d4e85dddcb1a7c15a661647c4ff708
Laus Vandresen <lausvan1205@gmail.com>
2019-05-06 11:56:16 -0300

    Atividade Remocao Id

--- src/main/java/br/com/edward/restfull/controller/PessoaController.java
+++ src/main/java/br/com/edward/restfull/controller/PessoaController.java
@@ -1,5 +1,8 @@
 package br.com.edward.restfull.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -12,20 +15,40 @@ import br.com.edward.restfull.model.PessoaModel;
 
 @RestController
 @RequestMapping("/pessoa")
-public class PessoaController {
+public class PessoaController { 
+	
+	private static List<PessoaModel> pessoas = new ArrayList<PessoaModel>();
 
 	@GetMapping("/param")
-    public PessoaModel param(@RequestParam String nome) {
-        return new PessoaModel(nome);
-    }
-	
-	@GetMapping("/path/{nome}")
-    public PessoaModel path(@PathVariable String nome) {
-        return new PessoaModel(nome);
-    }
-	
-	@PostMapping("/post")
-	public PessoaModel perguntar(@RequestBody PessoaModel model) {
-		return model;
+	public PessoaModel param(@RequestParam String nome) {
+		return new PessoaModel(nome);
 	}
+
+	@GetMapping("/path/{nome}")
+	public PessoaModel path(@PathVariable String nome) {
+		return new PessoaModel(nome);
+	}
+
+	@PostMapping("/post")
+	public PessoaModel perguntar(@RequestBody PessoaModel model) { 
+		pessoas.add(model);
+		return model;
+	}   
+	
+	@GetMapping("/remove")
+	public List<PessoaModel> remove(@RequestParam long id){   
+		for (PessoaModel pessoaModel : pessoas) {
+			if(pessoaModel.getId() == id) {
+				pessoas.remove(pessoaModel);
+			}
+		}		
+		return pessoas;
+	}
+	
+	@GetMapping("/listar")
+	public List<PessoaModel> listar() {
+		return pessoas;
+	}
+	
+	
 }
--- src/main/java/br/com/edward/restfull/model/PessoaModel.java
+++ src/main/java/br/com/edward/restfull/model/PessoaModel.java
@@ -5,19 +5,55 @@ import java.time.ZonedDateTime;
 public class PessoaModel {
 
 	private ZonedDateTime dataCriacao;
-	private String nome;
+	private String nome; 
+	private Double peso; 
+	private Integer idade; 
+	private String genero; 
+	private long id;
 
 	public PessoaModel() {
 		this.dataCriacao = ZonedDateTime.now();
 	}
-	
+
 	public PessoaModel(String nome) {
 		this();
 		this.nome = nome;
 	}
 	
+	
+
+	public PessoaModel(long id) {
+		super();
+		this.id = id;
+	}
+
+	public PessoaModel(String nome, Double peso, Integer idade, String genero, long id) {
+		super();
+		this.nome = nome;
+		this.peso = peso;
+		this.idade = idade;
+		this.genero = genero; 
+		this.id = id;
+	}
+
 	public String getNome() {
 		return nome;
+	} 
+	
+	public Double getPeso() {
+		return peso;
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public Integer getIdade() {
+		return idade;
+	}
+
+	public String getGenero() {
+		return genero;
 	}
 
 	public ZonedDateTime getDataCriacao() {
